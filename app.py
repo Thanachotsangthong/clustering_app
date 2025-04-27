f4db1eb80fedc3159b5e14d3c95805d1da396b2f
Thanachotsangthong <6531501053@lamduan.mfu.ac.th>
2025-04-27 15:35:40 +0700

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 """
 Created on Mon Apr 21 00:21:28 2025
 
-@author: parichikaphumikakrak
+@author: LAB
 """
 
 import streamlit as st
@@ -14,7 +14,7 @@ from sklearn.datasets import load_iris
 import matplotlib.pyplot as plt
 
 # Page title
-st.title("🔍 K-Means Clustering App with Iris Dataset by Parichika Phumikakrak")
+st.title("🔍 K-Means Clustering App with Iris Dataset by Thanachot Sangthong")
 
 # Load dataset
 iris = load_iris()
